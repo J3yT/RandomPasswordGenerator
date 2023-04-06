be5886c5d11295092b6db2378a3d1c7567f38ab4
Jason Tavera <taverajason7@gmail.com>
2023-04-05 22:36:20 -0500

    added most of the use cases in the java script file

--- script.js
+++ script.js
@@ -3,6 +3,37 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+let upperAlphabet = ['A','B','C','D','E','F','G','H', 'I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+let numbers = [0,1,2,3,4,5,6,7,8,9]
+
+let builtArray = []
+
+function generatePassword(){
+
+  if(confirm("Do you want your password to contain lowercase letters?")) {
+
+    builtArray = [...builtArray, ...lowerAlphabet]
+  }
+
+  let password = "";
+
+  let passwordLength =prompt("how long do you want the password")
+  for (let i = 0; i < passwordLength; i++) {
+
+    password += "x";
+
+
+  }
+
+
+
+  return password;
+
+
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
