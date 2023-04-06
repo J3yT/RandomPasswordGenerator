a84beebdc24dac54fff7a4ca8ca2d1d1603fb242
Jason Tavera <taverajason7@gmail.com>
2023-04-06 15:27:01 -0500

    made all the prompts to confirm and made the generator generate random items

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var generateBtn = document.querySelector("#generate");
 let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 let upperAlphabet = ['A','B','C','D','E','F','G','H', 'I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 let numbers = [0,1,2,3,4,5,6,7,8,9]
+let special = [ '!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[']
 
 let builtArray = []
 
@@ -14,25 +15,42 @@ function generatePassword(){
 
   if(confirm("Do you want your password to contain lowercase letters?")) {
 
-    builtArray = [...builtArray, ...lowerAlphabet]
+    builtArray = [...lowerAlphabet, ...builtArray]
   }
+
+  if(confirm("Do you want your password to contain uppercase letters")) {
+
+    builtArray = [...upperAlphabet, ...builtArray]
+  }
+
+  if(confirm("Do you want your password to contain numbers")){
+
+    builtArray = [...numbers, ... builtArray]
+  }
+
+  if(confirm("Do you want your password to contain special characters")){
+
+    builtArray = [...special, ...builtArray]
+
+  }
+
+  
+  let passwordLength =prompt("how long do you want the password")
 
   let password = "";
-
-  let passwordLength =prompt("how long do you want the password")
+  
   for (let i = 0; i < passwordLength; i++) {
 
-    password += "x";
-
+    let index = Math.floor(Math.random()*builtArray.length)
+    password += builtArray[index];
 
   }
-
-
 
   return password;
 
-
 }
+
+let generatedLetterIndex = Math.floor(Math.random()*129)
 
 function writePassword() {
   var password = generatePassword();
